a11980ae1216eb13df6c2c9e1f4b3825305a6380
as-is-prog <as-is-prog@outlook.jp>
2020-09-27 15:41:32 +0900

    first commit

---
+++ StringDecorationLibContract/Decorators/IStringDecorator.cs
@@ -0,0 +1,22 @@
+﻿namespace StringDecorationLibContract.Decorators
+{
+    /// <summary>
+    /// 文字に修飾を加える処理のインターフェース
+    /// </summary>
+    public interface IStringDecorator
+    {
+        /// <summary>
+        /// 任意の1文字に対して修飾を加える処理
+        /// </summary>
+        /// <param name="singleChar"></param>
+        /// <returns></returns>
+        string DecorateSingle(string singleChar);
+
+        /// <summary>
+        /// 文章全体に対して修飾を加える処理
+        /// </summary>
+        /// <param name="allText"></param>
+        /// <returns></returns>
+        string DecorateAll(string allText);
+    }
+}
