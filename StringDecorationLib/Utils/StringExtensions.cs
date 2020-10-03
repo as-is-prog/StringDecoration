f4d0a9fd733addefc34b9ca4b8aaa3947892df02
as-is-prog <as-is-prog@outlook.jp>
2020-10-03 20:52:20 +0900

    [add, fix] 文字列結合の拡張メソッド定義、nullチェック漏れ修正

---
+++ StringDecorationLib/Utils/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace StringDecorationLib.Utils
+{
+    /// <summary>
+    /// stringクラスに対する拡張メソッド群
+    /// </summary>
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// string.Joinをメソッドチェーンで呼び出せるようにしたもの
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="separator"></param>
+        /// <returns></returns>
+        public static string JoinString(this IEnumerable<string> str, string separator = "")
+        {
+            return string.Join(separator, str);
+        }
+    }
+}
